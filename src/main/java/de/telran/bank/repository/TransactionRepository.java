c3330f1a7140f9b95de76de50d0a1b28f9940ded
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2023-10-13 04:22:37 +0300

    2023-10-13 add description to TransactionRepository

--- src/main/java/de/telran/bank/repository/TransactionRepository.java
+++ src/main/java/de/telran/bank/repository/TransactionRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface TransactionRepository extends CrudRepository<Transaction,Long> {
+    //Возвращаем транзакции по Id клиента
     @Query("SELECT t FROM Transaction t JOIN t.debitAccount da JOIN da.client c WHERE c.id=?1")
     List<Transaction> findByClientId(Long clientId);
 
+    //Возвращаем транзакции по Фамилии менеджера
     @Query("SELECT t FROM Transaction t JOIN t.debitAccount da JOIN da.client c JOIN c.manager m " +
             "WHERE m.lastName LIKE %?1%")
     List<Transaction> findTransactionsByManagerId(String name);
